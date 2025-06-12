3bedab15d26304160f7ca2bc76919bb358f7c26e
lllou15 <98588320+lllou15@users.noreply.github.com>
2025-06-12 12:35:32 +0530

    fixed config

--- backend/config.py
+++ backend/config.py
@@ -4,8 +4,9 @@ import streamlit as st
 class Settings:
     GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
     DB_HOST = st.secrets["DB_HOST"]
-    DB_NAME = st.secrets["DB_NAME"]
+    DB_PORT = int(st.secrets["DB_PORT"])  # Ensure it's an integer
     DB_USER = st.secrets["DB_USER"]
+    DB_NAME = st.secrets["DB_NAME"]
     DB_PASSWORD = st.secrets["DB_PASSWORD"]
 
 
